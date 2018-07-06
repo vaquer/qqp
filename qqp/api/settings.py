ed128456844df38f611718901cc12697ceca36b0
Frank <akura11.tt@gmail.com>
2018-07-06 10:45:58 -0500

    Deploy kubernetes

--- qqp/api/settings.py
+++ qqp/api/settings.py
@@ -5,7 +5,6 @@ DATABASE = {
     'qqp': {
         'host': os.environ.get('DATABASE_HOST'),
         'username': os.environ.get('DATABASE_USERNAME'),
-        'user': os.environ.get('DATABASE_USER'),
         'password': os.environ.get('DATABASE_PASSWORD'),
         'port': int(os.environ.get('DATABASE_PORT', '5432'))
     }
